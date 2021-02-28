a97d5b9f45e37a40d292fb070bd244066f1a9cb1
GitHub Action <action@github.com>
2021-02-28 03:20:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/yashtotale/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yashtotale/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yashtotale.intellijplugin.services
 
+import com.github.yashtotale.intellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
