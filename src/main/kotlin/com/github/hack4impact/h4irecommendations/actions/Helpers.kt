cd0d19fc3a339c83688c27a10783617268430b21
Yash Totale <totaleyash@gmail.com>
2021-03-01 15:29:45 -0800

    Added Create Template File Helper

--- src/main/kotlin/com/github/hack4impact/h4irecommendations/actions/Helpers.kt
+++ src/main/kotlin/com/github/hack4impact/h4irecommendations/actions/Helpers.kt
@@ -1,13 +1,19 @@
 package com.github.hack4impact.h4irecommendations.actions
 
+import com.intellij.ide.BrowserUtil
+import com.intellij.ide.actions.OpenFileAction
 import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.Nullable
+import java.io.File
+import java.net.URI
 
 object Helpers {
     var notificationGroup = NotificationGroup("Hack4Impact", NotificationDisplayType.BALLOON, true)
@@ -29,5 +35,23 @@ object Helpers {
         }
 
         return file
+    }
+
+    fun createTemplateFile(name: String, filePath: String, fileContent: String, docsUrl: String, project: Project?) {
+        File(filePath).writeText(fileContent, Charsets.UTF_8)
+        notificationGroup.createNotification(
+            "$name template successfully created!",
+            NotificationType.INFORMATION
+        ).addAction(object : AnAction("View Template") {
+            override fun actionPerformed(e: AnActionEvent) {
+                if (project != null) {
+                    OpenFileAction.openFile(filePath, project)
+                }
+            }
+        }).addAction(object : AnAction("View Docs") {
+            override fun actionPerformed(e: AnActionEvent) {
+                BrowserUtil.browse(URI.create(docsUrl))
+            }
+        }).notify(project)
     }
 }
--- src/main/kotlin/com/github/hack4impact/h4irecommendations/actions/config/CreateEditorConfigConfigurationTemplate.kt
+++ src/main/kotlin/com/github/hack4impact/h4irecommendations/actions/config/CreateEditorConfigConfigurationTemplate.kt
@@ -3,10 +3,40 @@ package com.github.hack4impact.h4irecommendations.actions.config
 import com.github.hack4impact.h4irecommendations.actions.Helpers
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 
 class CreateEditorConfigConfigurationTemplate : AnAction() {
+    var templateContent = """
+        # EditorConfig helps developers define and maintain consistent coding styles between different editors and IDEs
+
+        root = true
+
+        [*]
+        end_of_line = lf
+        charset = utf-8
+        trim_trailing_whitespace = true
+        insert_final_newline = true
+        indent_style = space
+        indent_size = 2
+    """.trimIndent()
+
     override fun actionPerformed(e: AnActionEvent) {
         var project = e.project
-        Helpers.getSingleFolder(project)
+        var file = Helpers.getSingleFolder(project)
+
+        if (file !== null) {
+            createTemplate(file, project)
+        }
+    }
+
+    fun createTemplate(file: VirtualFile, project: Project?) {
+        Helpers.createTemplateFile(
+            "EditorConfig",
+            file.path + "/.editorconfig",
+            templateContent,
+            "https://editorconfig.org",
+            project
+        )
     }
 }
